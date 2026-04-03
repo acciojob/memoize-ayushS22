ef91b9b0a91436b837ad53b6a4dbe6a14aae6103
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-04-03 15:48:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-function memoize(callback, resolver = JSON.stringify) {
-   
-  }
-  
-  module.exports = memoize;
-  
+function memoize(callback, resolver) {
+  const cache = new Map();
+
+  // function to generate key
+  const getKey = (args) => {
+    if (resolver) {
+      return resolver(...args); // custom resolver
+    }
+    return JSON.stringify(args); // default behavior
+  };
+
+  const memoized = function (...args) {
+    const key = getKey(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = callback(...args);
+    cache.set(key, result);
+    return result;
+  };
+
+  // clear cache
+  memoized.clear = function () {
+    cache.clear();
+  };
+
+  // delete specific cache entry
+  memoized.delete = function (...args) {
+    const key = getKey(args);
+    cache.delete(key);
+  };
+
+  // check if cache has entry
+  memoized.has = function (...args) {
+    const key = getKey(args);
+    return cache.has(key);
+  };
+
+  return memoized;
+}
+
+module.exports = memoize;
